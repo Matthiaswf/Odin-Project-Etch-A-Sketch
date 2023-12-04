b99b9e81f6535c3ddc9e686d52366b5b10a40bfb
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2023-12-04 12:59:11 +0900

    Js elements to divs and added correct classes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let containerDiv = document.createElement('container');
-containerDiv.textContent = 'Hello World';
-document.body.appendChild(containerDiv);
+let container = document.createElement('div');
+container.classList.add('container');
+let canvas = document.createElement('div');
+canvas.classList.add('canvas');
+document.body.appendChild(container);
+container.appendChild(canvas);
